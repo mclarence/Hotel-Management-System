175f191903a612e0d3c9f90fb19fce8923d8e18d
Clarence <c.mes@cm0.dev>
2023-09-12 14:23:34 +1000

    add tickets page

--- apps/hotel-management-system/src/app/pages/tickets/Tickets.tsx
+++ apps/hotel-management-system/src/app/pages/tickets/Tickets.tsx
@@ -1,6 +1,32 @@
 import { useEffect } from "react";
 import { useAppDispatch } from "../../redux/hooks";
 import appStateSlice from "../../redux/slices/AppStateSlice";
+import { Button, Stack } from "@mui/material";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'Ticket #', width: 70 },
+    { field: 'description', headerName: 'description', width: 130 },
+    { field: 'room', headerName: 'Room', width: 130 },
+    { field: 'guest', headerName: 'Guest', width: 130 },
+    { field: 'assignedTo', headerName: 'Assigned To', width: 130 },
+    { field: 'status', headerName: 'Status', width: 130},
+    // add a column for a button to open the ticket
+
+    { field: 'actions', headerName: 'Actions', width: 150, renderCell: (params: any) => (
+        <strong>
+            <Button variant="outlined">
+                More Details
+            </Button>
+        </strong>
+    )},
+];
+
+const rows = [
+    { id: 1, description: 'Broken TV', room: 'Jon', guest: 'Snow', assignedTo: 'Jon', status: 'Open' },
+    { id: 2, description: 'Leaking Sink', room: 'Jon', guest: 'White', assignedTo: 'Mark', status: 'Open' },
+
+];
 
 export const Tickets = () => {
 
@@ -9,10 +35,32 @@ export const Tickets = () => {
     useEffect(() => {
         dispatch(appStateSlice.actions.setAppBarTitle('Tickets'));
     }, []);
-    
+
     return (
         <>
-            This is the Tickets page
+            <Stack direction={'column'} gap={2}>
+                <Stack direction={'row'} gap={2}>
+                    <Button variant="contained" color="success">
+                        New Ticket
+                    </Button>
+                    <Button variant="contained" color="error">
+                        Close Ticket
+                    </Button>
+                </Stack>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    initialState={{
+                        pagination: {
+                            paginationModel: { page: 0, pageSize: 5 },
+                        },
+                    }}
+                    pageSizeOptions={[5, 10]}
+                    checkboxSelection
+                />
+
+            </Stack>
+
         </>
     )
 }
