8cf0f2496f1fca9a185eb0ec2a11effd29b90c0e
CharlieZhan2002 <1762516664@qq.com>
2023-09-13 20:23:39 +1000

    test pipeline change

--- apps/hotel-management-system/src/app/pages/dashboard/Dashboard.tsx
+++ apps/hotel-management-system/src/app/pages/dashboard/Dashboard.tsx
@@ -14,7 +14,7 @@ export const Dashboard = () => {
     return (
         <div>
             <Button variant="outlined">Outlined</Button>
-            Test change 2
+            Test change
         </div>
     )
 }
