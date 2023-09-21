8d012aa462542976657d6cfc06bf0894064062b3
Clarence Mesina <me@clarence.id.au>
2023-09-21 19:19:28 +1000

    add missing files

---
+++ libs/models/src/lib/enums/PriceUnits.ts
@@ -0,0 +1,7 @@
+export enum PriceUnits {
+    day = 'day',
+    week = 'week',
+    month = 'month',
+    year = 'year',
+    night = 'night'
+}
---
+++ libs/models/src/lib/enums/RoomStatuses.ts
@@ -0,0 +1,7 @@
+export enum RoomStatuses {
+    Available = 'Available',
+    Unavailable = 'Unavailable',
+    Reserved = 'Reserved',
+    Occupied = 'Occupied',
+    OutOfService = 'Out of service',
+}
